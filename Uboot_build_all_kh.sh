c830130ac7e409177c197352775102613169247b
kuihong-Embedded <504059700@qq.com>
2023-09-07 21:34:24 +0800

    Support Board mx6ull_kh

--- Uboot_build_all_kh.sh
+++ Uboot_build_all_kh.sh
@@ -4,7 +4,8 @@
 
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_ddr512_emmc_defconfig
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_ddr512_emmc_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- kh_mx6ull_14x14_ddr512_emmc_defconfig
 
 make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j4
 
